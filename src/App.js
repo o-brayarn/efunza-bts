96fc77704896c9711720eb4bd072fca84e86c79f
constatnce <luvunoconstance795@gmail.com>
2023-10-09 00:50:39 +0300

    Initial commit login,register, about pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import Header from './components/header/Header';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import About from './pages/about/About';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee, faUser, faLock, faEye, faInbox } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+library.add(fab, faCheckSquare, faCoffee, faUser, faLock, faEye, faInbox);
+
+ 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import {Link} from'react-scroll';
+const Header = () => {
+    return (
+      <nav classNanpm install react-scrollme="navbar">
+           <h2>EBTS</h2>
+           <Link className="desktopMenuListItem"></Link>
+      </nav>
+    )
+  }
+  
+  export default Header
