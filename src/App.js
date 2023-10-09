b509d66b6ac7ea8a7518213e94d583c4285c971f
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-10-09 20:36:23 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-
-import Header from './components/header/Header';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import About from './pages/about/About';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faUser, faLock, faEye, faInbox } from '@fortawesome/free-solid-svg-icons';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Contacts } from "./pages";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Contacts, Login, About, Register } from "./pages";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCheckSquare,
+  faCoffee,
+  faUser,
+  faLock,
+  faEye,
+  faInbox,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, faCheckSquare, faCoffee, faUser, faLock, faEye, faInbox);
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
