d8c67dc70f2ccda7e8646c79b382e57946f0dc49
constatnce <luvunoconstance795@gmail.com>
2023-10-17 05:05:13 -0700

    Updating changes in login,register and about us page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,89 +1,73 @@
-import React from "react";
-import "./about.css";
-
-import cittyhoppa from "../../assets/cittyhoppa.png";
-
+import React from 'react'
+import './about.css'
+import cittyhoppa from "../../assets/image3.png"
 const About = () => {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-12 mb-5 hero-about">
-          <div className="profile-image text-center">
-            <h1> About Us</h1>
-          </div>
+      <div className='row'>
+      <div className="col-lg-12 mb-5 px-0" >
+         <div className="profile-image ">
+         <div class="background-image"></div>
+        <div class="overlay"></div>
+        <div class="content text-center">
+            <h1 className='fw-bolder'>About Us</h1>
         </div>
-
-        <div className="row bg-light align-content-sm-center">
-          <div className="col-md-4 col-lg-4 d-flex mb-3">
-            <div className="card p-3 w-100">
-              <h3>Our vision</h3>
-              <hr />
-              <p>
-                To be the go to traveling company in Kenya and to make traveling
-                affordable for everyone.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 col-lg-4 d-flex mb-3">
-            <div className="card p-3 w-100">
-              <h3>Our Mission</h3>
-              <hr />
-              <p>
-                Our mission is to redefine the way you experience bus traveling.
-                We are dedicated to providing you with seamless, safe and
-                convenient booking solutions that empower your journeys.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 col-lg-4 d-flex mb-3">
-            <div className="card p-3 w-100">
-              <h3>Company values</h3>
-              <hr />
-              <p>
-                Integrity.
-                <br />
-                Reliability. <br />
-                Safety first. <br />
-                Customer satisfaction
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row bg-light align-content-sm-center mt-5">
-          <div className="col-lg-4 col-md-6 col-m-12">
-            <h3
-              className=" text-white border border-0 rounded p-2 mb-4"
-              style={{
-                width: "fit-content",
-                backgroundColor: "#1c23cb",
-              }}
-            >
-              About Us
-            </h3>
-            <p>
-              We are passionate about making your travel experience smooth,
-              convenient and memorable. Whether you're a daily commuter or an
-              adventure seeker exploring new destinations we've got you covered
-            </p>
-            <a href="/contact" className="btn btn-dark mt-3">
-              {" "}
-              Get in Touch
-            </a>
-          </div>
-          <div className="col-lg-8 col-md-6 col-sm-12 ">
-            <div className="profile-image text-center">
-              <img
-                src={cittyhoppa}
-                alt="images i.png"
-                className="Img img-fluid w-100"
-              />
-            </div>
-          </div>
+       </div>
         </div>
       </div>
+     
+      <div className="container">
+        <div className="row company bg-light align-content-sm-center">
+    <div className="col-md-4 col-lg-4 d-flex mb-3">
+      <div className='box p-4 w-100'>
+      <h3 className='text-center'>Our vision</h3>
+      <p>To be the go to traveling company in Kenya and to make traveling affordable for everyone.</p>
     </div>
-  );
-};
+    </div>
+    <div className="col-md-4 col-lg-4 d-flex mb-3">
+      <div className='box p-4 w-100'>
+      <h3 className='text-center'>Our Mission</h3>
+      <p>Our mission is to redefine the way you experience bus traveling. We are dedicated to providing you with seamless, safe and convenient booking solutions that empower your journeys.</p>
+    </div>
+    </div>
+    <div className="col-md-4 col-lg-4 d-flex mb-3" >
+      <div className='box p-4 w-100'>
+        <h3 className='text-center'>Company values</h3>
+        <p>Integrity.<br/> 
+        Reliability. <br/>
+        Safety first. <br/>
+        Customer satisfaction</p>
+        </div>
+      </div>
+      </div>
+    
+        <div class="row bg-light align-content-sm-center mt-5 aboutcontent">
+        <div class="col-lg-7  col-md-8 col-m-12">
+          <div className='abouth4 d-flex justify-content-around  mb-3'>
+        <h4 class="text-white">About-us</h4>
+        </div>
+          <p>We are passionate about making your travel experience smooth,
+             convenient and memorable. Whether you're a daily commuter or an adventure 
+             seeker exploring new destinations we've got you covered</p>
+             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quae dolorem illum beatae quidem ex harum consectetur necessitatibus minus, 
+    provident, vitae optio. At sapiente deleniti doloremque nemo nobis similique,
+     tenetur incidunt Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quae dolorem illum beatae quidem ex harum consectetur necessitatibus minus, 
+    provident, vitae optio. At sapiente deleniti doloremque nemo nobis similique, tenetur incidunt</p>
+          </div>
+        <div class="col-lg-5 col-md-4 col-sm-12 ">
+        <div className="profile-image text-center">
+          <img src={cittyhoppa} alt="images 3.png" className="Img img-fluid"/>
+        </div>
+        </div>
+      </div>
 
-export default About;
+      </div>
+      </div>
+      
+    
+
+        )
+}
+export default About
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,72 +1,60 @@
-import React from "react";
-import "./login.css";
-import LogoImg from "../../assets/logo.png";
-import bg from "../../assets/Bus.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react'
+import './login.css'
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="container mt-5 overflow-y-hidden ">
-      <div className="row hero-login">
-        <div className="col-lg-2 col-md-2"></div>
-        <div className="col-lg-4 busyangu d-flex align-items-center">
-          <div className="bus-image">
-            <img src={bg} alt="Bus" className="img-fluid w-100" />
-          </div>
+    <div className="container-fluid  overflow-y-hidden bg-dark h-100 w-100 pt-5">
+      <div className="row hero-login mt-5">
+        <div className='col-lg-2 col-md-2'></div>
+        <div className="col-lg-4 px-0 bgimg bg-white d-flex align-items-center">
+        <div class="background-image"></div>
+        <div class="overlay"></div>
+         <div className="content align-self-center text-center">
+             <h2 className='h5 fw-bolder'>EFUNZA BUS <br/>TICKETING SYSTEM</h2>
         </div>
-        <div className="col-lg-4 bg-dark d-flex">
+        </div>
+        <div className='col-lg-4 bg-light'>
           <div className="py-5">
-            <div className="login-form text-center">
-              <img
-                src={LogoImg}
-                alt="logo"
-                className="LogoImg img-fluid w-25 mt-2 text-center"
-              />
-              <h1 className="text-white">Login</h1>
-              <small className="text-white">Sign in to your account</small>
-            </div>
-            <form className="form  p-4 mb-5">
-              <div className="input-group">
-                <span className="input-group-text">
-                  <FontAwesomeIcon icon="user" />{" "}
-                </span>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Username"
-                />
-              </div>
-              <div className="input-group my-3">
-                <span className="input-group-text">
-                  <FontAwesomeIcon icon="lock" />
-                </span>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                />
-                <span className="input-group-text">
-                  <FontAwesomeIcon icon="eye" />{" "}
-                </span>
-              </div>
-
-              <a href="signin" className="btn btn-primary mt-4 content-center">
-                Sign Up{" "}
-              </a>
-              <p className="text-white float-end">forgot password?</p>
-              <br />
-              <Link to="/register" className="text-white">
-                {" "}
-                Register new account{" "}
-              </Link>
-            </form>
-          </div>
+     <div className="login-form text-center">
+       <h1 className="text-dark">Login</h1>
+        <small className='text-dark'>Sign in to your account</small>
         </div>
-        <div className="col-lg-2 col-md-2"></div>
+        <form className='form  p-4 mb-5 '>
+        <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email address"/>
+</div>
+<div class="mb-3">
+  <label for="Password" class="form-label">Password</label>
+  <input type="password" class="form-control" id="password" placeholder="Password"/>
+</div>
+
+        <div className='form-row d-flex justify-content-between'>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+        <label class="form-check-label" for="flexCheckChecked">
+          Show Password
+        </label>
+         </div>
+             <p className="text-dark">Forgot Password?</p>
+         </div>
+      
+        <input type='submit' className='btn btn-primary mt-4 content-center fw-1 w-100 my-2 text-capitalize'value='LOGIN'/>
+        
+        <br/>
+        <div className='text-center'>
+        Don't have an account? &nbsp;
+        <Link to="/register" className='text-primary'> Register   </Link>
+        </div>
+        
+       </form>
+       </div>
+      </div>
+      <div className='col-lg-2 col-md-2'></div>
       </div>
     </div>
-  );
-};
+    )
+}
 
-export default Login;
+export default Login
